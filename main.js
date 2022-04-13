3c7a03e84dfee165c9f353ceaf88325ca61f7e49
Feiruz Khouri <feiruz25@hotmail.com>
2022-04-13 13:53:01 +0200

    try usuario 0

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const url = "https://randomuser.me/api/?results=10";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
+    console.log(data.results);
+    const usuarios = [];
+    const city = data.results[0].location.city;
+    const name = data.results[0].name.first + " " + data.results[0].name.last;
+    const image = data.results[0].picture.medium;
+    const userName = data.results[0].login.username;
+    console.log(userName);
+  });
